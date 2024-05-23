f2ef648a78b561b30f8deff4a7d393d08a8a6db3
hanane <hananelahssini22@gmail.com>
2024-05-23 16:47:11 +0100

    vv2

--- src/app/customers/customers.component.ts
+++ src/app/customers/customers.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { catchError, Observable, throwError } from 'rxjs';
+import { Customer } from '../model/customer.model';
+import { CustomerService } from '../services/customer.service';
 
 @Component({
   selector: 'app-customers',
   templateUrl: './customers.component.html',
   styleUrl: './customers.component.css'
 })
-export class CustomersComponent {
+export class CustomersComponent implements OnInit {
+  customers! : Observable<Array<Customer>>;
+  errorMessage!: string;
+  constructor(public customerService:CustomerService){
+
+  }
+  ngOnInit(): void {
+    this.customers=this.customerService.getCustomers().pipe(
+      catchError(err => {
+        this.errorMessage=err.message;
+        return throwError(err);
+      })
+    );
+    
+  }
 
 }
